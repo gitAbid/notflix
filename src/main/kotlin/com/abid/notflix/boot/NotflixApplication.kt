da7c9de6df66d36411836a316a13e6536898b2c6
Abid Hasan <abid.northsouth@gmail.com>
2020-04-04 10:49:14 +0600

    Controller updated

--- src/main/kotlin/com/abid/notflix/boot/NotflixApplication.kt
+++ src/main/kotlin/com/abid/notflix/boot/NotflixApplication.kt
@@ -1,9 +1,11 @@
-package com.abid.notflix
+package com.abid.notflix.boot
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.abid.notflix")
 class NotflixApplication
 
 fun main(args: Array<String>) {
