9d2b6b1cfa9f40019d51a5dc2df097e083760522
Abid Hasan <abid.northsouth@gmail.com>
2020-04-04 10:59:08 +0600

    Update controller message and response

--- src/main/kotlin/com/abid/notflix/controller/HomeController.kt
+++ src/main/kotlin/com/abid/notflix/controller/HomeController.kt
@@ -1,12 +1,16 @@
 package com.abid.notflix.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping("/")
 class HomeController {
 
+    @GetMapping
+    @ResponseBody
     fun home(): String {
         return "Welcome To Notflix"
     }
