e18a468f78040032144a17821c81c9cf6d3e445e
Abid Hasan <abid.northsouth@gmail.com>
2020-04-04 09:21:39 +0600

    project setup

---
+++ src/main/kotlin/com/abid/notflix/NotflixApplication.kt
@@ -0,0 +1,11 @@
+package com.abid.notflix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotflixApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotflixApplication>(*args)
+}
---
+++ src/test/kotlin/com/abid/notflix/NotflixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abid.notflix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotflixApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
